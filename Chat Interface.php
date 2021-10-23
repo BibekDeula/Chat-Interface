34f9aa2c6e83f4e7b179b6dd47a71eb7265c4778
bibek deula <bbkdeula@gmail.com>
2021-10-23 17:58:32 +0545

    Added

---
+++ Chat Interface.php
@@ -0,0 +1,76 @@
+<!-- Based on Chat Interface by Florin Pop (2019)
+see: https://www.florin-pop.com/blog/2019/04/chat-interface/ -->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="bbk.css" />
+    <title>Chat Interface</title>
+  </head>
+  <body>
+    <h2>minions</h2>
+    <div class="chat-container">
+      <ul class="chat">
+        <li class="message left">
+          <img
+            class="avatar"
+            src="bk.jpg"
+            alt=""
+          />
+          <p>Hello,whats up!!</p>
+        </li>
+        <li class="message right">
+          <img
+            class="avatar"
+            src="bbk.jpg"
+            alt=""
+          />
+          <p>Hey!! How are You?? sorry i forgot your name.</p>
+        </li>
+        <li class="message left">
+          <img
+            class="avatar"
+            src="bk.jpg"
+            alt=""
+          />
+          <p>Its me minions ..</p>
+        </li>
+        <li class="message right">
+          <img
+            class="avatar"
+            src="bbk.jpg"
+            alt=""
+          />
+          <p>Hey !!How are you dude?? :-)</p>
+        </li>
+        <li class="message left">
+          <img
+            class="avatar"
+            src="bk.jpg"
+            alt=""
+          />
+          <p>lets hangout tomorrow bro..</p>
+        </li>
+        <li class="message right">
+          <img
+            class="avatar"
+            src="bbk.jpg"
+            alt=""
+          />
+          <p>Sure</p>
+        </li>
+        <li class="message right">
+          <img
+            class="avatar"
+            src="bbk.jpg"
+            alt=""
+          />
+          <p>......</p>
+        </li>
+      </ul>
+      <input type="text" class="text_input" placeholder="Message..." />
+    </div>
+  </body>
+</html>
